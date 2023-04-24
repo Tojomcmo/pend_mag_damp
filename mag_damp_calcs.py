9982e1919fdd2fc1ecb45033ade6051895ba3c3a
tojomcmo <tojomcmo@gmail.com>
2023-04-23 20:36:22 -0400

    renamed documents, fixed math errors, added more test data

---
+++ mag_damp_calcs.py
@@ -0,0 +1,71 @@
+import pend_funcs as pf
+import numpy as np
+
+pend_exp_1 = pf.pend_experiment()
+num_experiments = 4
+
+description = [['round lg 1 mag 10.25 height'],
+               ['round lg 1 mag 15 height'],
+               ['rect 1 mag 10.75 height'],
+               ['rect 1 mag 14.5 height'],
+               ['rect 1 mag 14.5 height no plastic']]
+
+mag_mass = [0.00263,
+            0.00263,
+            0.00423,
+            0.00423,
+            0.00423]
+ramp_angle = [np.arcsin(10.25/16), 
+              np.arcsin(15/16),
+              np.arcsin(10.75/16),
+              np.arcsin(14.5/16),
+              np.arcsin(14.5/16)]
+ramp_dist = [0.4064,
+             0.4064,
+             0.4064,
+             0.4064,
+             0.4064]
+mag_offset = [1.5875,
+              1.5875,
+              1.5875,
+              1.5875,
+              0]
+no_damp_values = [[51/120, 52/120, 51/120, 50/120],
+                  [38/120, 36/120, 37/120, 38/120, 39/120, 36/120],
+                  [46/120, 49/120, 48/120, 50/120, 47/120],
+                  [37/120, 37/120, 37/120],
+                  []]
+damp_values = [[93/120, 95/120, 95/120], 
+               [54/120, 53/120, 60/120, 57/120, 56/120, 57/120],
+               [153/120, 158/120, 154/120],
+               [100/120, 110/120, 112/120, 109/120],
+               []]
+
+exp_3_xdot_ss = 5/(45/120) * 25.4/1000
+exp_4_xdot_ss = 5/(32/120) * 25.4/1000
+
+pend_exps       = [None] * num_experiments
+friction_coeffs = [None] * num_experiments
+damping_coeffs  = [None] * num_experiments
+
+for idx in range(num_experiments):
+    pend_exps[idx] = pf.pend_experiment()
+    pend_exps[idx].description           = description[idx]    
+    pend_exps[idx].magnet_mass_kg        = mag_mass[idx]
+    pend_exps[idx].ramp_angle_rad        = ramp_angle[idx]
+    pend_exps[idx].ramp_distance_m       = ramp_dist[idx]
+    pend_exps[idx].magnet_standoff_mm    = mag_offset[idx]
+    pend_exps[idx].t_final_no_damp_s_vec = no_damp_values[idx]
+    pend_exps[idx].t_final_damp_s_vec    = damp_values[idx]
+
+    friction_coeffs[idx] = pend_exps[idx].calculate_friction_coefficient()
+    damping_coeffs[idx]  = pend_exps[idx].calculate_damping_coefficient()
+
+
+print('friction coefficients:  ', friction_coeffs)
+
+print('damping coefficients:  ', damping_coeffs)
+
+print('exp_3_damp_coeff_from_ss', pend_exps[2].calc_steady_state_damping_coefficient(exp_3_xdot_ss))
+print('exp_4_damp_coeff_from_ss', pend_exps[3].calc_steady_state_damping_coefficient(exp_4_xdot_ss))
+print('exp_4_override_mu', pend_exps[3].calculate_damping_coefficient(mu_k = 0.23))
--- mag_damp_funcs.py
+++ mag_damp_funcs.py
@@ -2,13 +2,14 @@ import numpy as np
 
 class pend_experiment(object):
     def __init__(self, 
-                 magnet_mass_kg = None, 
-                 ramp_angle_rad = None, 
-                 ramp_distance_m = None,
-                 magnet_standoff_mm = None, 
+                 description           = None,
+                 magnet_mass_kg        = None, 
+                 ramp_angle_rad        = None, 
+                 ramp_distance_m       = None,
+                 magnet_standoff_mm    = None, 
                  t_final_no_damp_s_vec = [], 
-                 t_final_damp_s_vec = []):
-        
+                 t_final_damp_s_vec    = []):
+        self.description           = description
         self.magnet_mass_kg        = magnet_mass_kg
         self.ramp_angle_rad        = ramp_angle_rad
         self.ramp_distance_m       = ramp_distance_m
@@ -31,23 +32,39 @@ class pend_experiment(object):
     def calculate_friction_coefficient(self):
         xf = self.ramp_distance_m
         th = self.ramp_angle_rad
+        g  = self.gravity_m_s2
         tf = self.get_avg_t_final('no_damp')
-        mu_k = (-2 * xf) / (9.81 * tf**2) + np.sin(th) / np.cos(th)
+        # equation for mu_k derived from ramp problem
+        mu_k = ((-2 * xf) / (g * tf**2) + np.sin(th)) / np.cos(th)
         return mu_k
     
-    def calculate_damping_coefficient(self):
+    def calculate_damping_coefficient(self, mu_k = None):
         xf   = self.ramp_distance_m
         m    = self.magnet_mass_kg
         g    = self.gravity_m_s2
         th   = self.ramp_angle_rad
         tf   = self.get_avg_t_final('damp')
-        mu_k = self.calculate_friction_coefficient()
+        if mu_k == None:
+            mu_k = self.calculate_friction_coefficient()    
         a    = g * (np.sin(th) - mu_k * np.cos(th))
 
+        # initialize comparison xf bias too far to initiate while loop
         x_f_check = xf + 1 
+        # initialize damping coefficient to zero
         b         = 0
-
+        # increase b, calculate expected final x distance given experimental time
+        # iterate until x_f_check matches or exceeds experimental distance
         while x_f_check >= xf:
             b = b + 1e-5
+            # equation for x_f_check derived from diff eq describing ramp problem with linear damping
             x_f_check = (a * m**2)/(b**2) * np.exp((-b)/(m)*tf) + (a * m)/(b) * tf - (a * m**2)/(b**2)
+        return b
+    
+
+    def calc_steady_state_damping_coefficient(self, x_dot_measured_m_s):
+        th   = self.ramp_angle_rad
+        g    = self.gravity_m_s2
+        m    = self.magnet_mass_kg
+        mu_k = self.calculate_friction_coefficient()
+        b    = (m * g)*(np.sin(th) - mu_k * np.cos(th))/x_dot_measured_m_s
         return b
